8bd033e00d8a3a6d8cd1a9501c76b9fee0b7a7b3
dex black <dex@dexblack.net>
2018-11-12 11:21:18 +1100

    Consistent quotes

--- cramclub/cramconst.py
+++ cramclub/cramconst.py
@@ -7,10 +7,10 @@ APP_NAME = "cramclub"
 RETRY_TIME = 60   # seconds
 
 CUSTOM_FIELDS = 'custom_fields'
-CUSTOM_FIELD_CONTACTID = "2184"
+CUSTOM_FIELD_CONTACTID = '2184'
 
-CUSTOM_FIELD_FEDERAL = "2170"
-CUSTOM_FIELD_STATE = "2171"
+CUSTOM_FIELD_FEDERAL = '2170'
+CUSTOM_FIELD_STATE = '2171'
 
 def dot_or_nothing(instance):
     return '.' + instance if instance else ''
