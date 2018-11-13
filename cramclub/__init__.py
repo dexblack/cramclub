850cf95ace1cb31a035bd68a3f58b031e5668cb3
dex black <dexblack254@gmail.com>
2018-11-13 15:15:59 +1100

    Package level import doesn't help fix pylint import-error

--- cramclub/__init__.py
+++ cramclub/__init__.py
@@ -2,14 +2,14 @@
 Package level import
 """
 
-__all__ = [
-    'CallHub',
-    'CramLog',
-    'CramPull',
-    'CramIo'
-]
+# __all__ = [
+#     'CallHub',
+#     'CramLog',
+#     'CramPull',
+#     'CramIo'
+# ]
 
-from cramclub.cramclub.callhub import CallHub
-from cramclub.cramclub.cramlog import CramLog
-from cramclub.cramclub.crampull import CramPull
-from cramclub.cramclub.cramio import CramIo
+# from cramclub.cramclub.callhub import CallHub
+# from cramclub.cramclub.cramlog import CramLog
+# from cramclub.cramclub.crampull import CramPull
+# from cramclub.cramclub.cramio import CramIo
